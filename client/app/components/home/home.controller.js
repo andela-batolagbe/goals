ac35aaf473ad097c2a3eaa117c9064c5f376b5be
Atolagbe Abisoye <atolagbe.bisoye@andela.com>
2017-03-16 14:16:07 +0100

    Add show goals list functionality

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -2,21 +2,18 @@
 class HomeController {
   constructor($scope, HomeService) {
     this.scope = $scope;
-    this.fs = fs;
     this.goals = [];
     this.service = HomeService;
-  }
-
-  onInit() {
+    this.$onInit = () => {
     this.service.getGoals()
     .then((data) => {
-      console.log(data)
       this.goals = data;
     })
     .catch((err) => {
       console.log(err);
     })
   };
+  }
 
 }
 
--- client/app/components/home/home.service.js
+++ client/app/components/home/home.service.js
@@ -1,24 +1,27 @@
 
 class HomeService {
-  constructor($http, $q) {
-    this.http = $http;
-    this.q = $q;
+  constructor($q, $http) {
+    this.$q = $q;
+    this.$http = $http;
   }
 
-  getGoals() {
-    this.q((resolve, reject) => {
-      this.http({
-        method: 'GET',
-        url: '../../../goals.json'
-      }, (success) => {
-        resolve(success);
-      },
-      (err) => {
-        reject(err);
+  getGoals(data) {
+
+    return this.$q((resolve, reject) => {
+      this.$http({
+        url: '../../../goals.json',
+        method: 'GET'
       })
+      .then((response) => {
+        resolve(response.data);
+        },
+        (response) => {
+          reject(response.data);
+      });
     })
   }
-}
+};
 
 HomeService.$inject = ['$q', '$http'];
+
 export default HomeService;
